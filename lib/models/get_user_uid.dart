0f49a67f2d6b73e5d886e86f7546fe373702c596
micsarawut <65011212206@msu.ac.th>
2025-04-30 01:33:19 +0700

    micup

---
+++ lib/models/get_user_uid.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+// To parse this JSON data, do
+//
+//     final getUserUid = getUserUidFromJson(jsonString);
+
+
+GetUserUid getUserUidFromJson(String str) => GetUserUid.fromJson(json.decode(str));
+
+String getUserUidToJson(GetUserUid data) => json.encode(data.toJson());
+
+class GetUserUid {
+    int uid;
+    String name;
+    String email;
+    String password;
+    String height;
+    String weight;
+    String shirtSize;
+    String chest;
+    String waistCircumference;
+    String hip;
+    String personalDescription;
+    String profileImage;
+
+    GetUserUid({
+        required this.uid,
+        required this.name,
+        required this.email,
+        required this.password,
+        required this.height,
+        required this.weight,
+        required this.shirtSize,
+        required this.chest,
+        required this.waistCircumference,
+        required this.hip,
+        required this.personalDescription,
+        required this.profileImage,
+    });
+
+    factory GetUserUid.fromJson(Map<String, dynamic> json) => GetUserUid(
+        uid: json["uid"],
+        name: json["name"],
+        email: json["email"],
+        password: json["password"],
+        height: json["height"],
+        weight: json["weight"],
+        shirtSize: json["shirt_size"],
+        chest: json["chest"],
+        waistCircumference: json["waist_circumference"],
+        hip: json["hip"],
+        personalDescription: json["personal_description"],
+        profileImage: json["profile_image"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "uid": uid,
+        "name": name,
+        "email": email,
+        "password": password,
+        "height": height,
+        "weight": weight,
+        "shirt_size": shirtSize,
+        "chest": chest,
+        "waist_circumference": waistCircumference,
+        "hip": hip,
+        "personal_description": personalDescription,
+        "profile_image": profileImage,
+    };
+}
