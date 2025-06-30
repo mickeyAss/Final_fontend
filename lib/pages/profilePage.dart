ac083490363aea87d59f52464b1988b15f343106
Apxdlxz <aern123464@gmail.com>
2025-07-01 00:36:56 +0700

    earnup_myprofile

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -1,11 +1,12 @@
 import 'dart:developer';
-
 import 'package:fontend_pro/models/get_user_uid.dart';
+import 'package:fontend_pro/pages/user_my_bookmarks_tab.dart';
+import 'package:fontend_pro/pages/user_my_likes_tab.dart';
+import 'package:fontend_pro/pages/user_my_post_tab.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:fontend_pro/config/config.dart';
 import 'package:fontend_pro/pages/edit_profile.dart';
-import 'package:fontend_pro/pages/mainPage.dart';
 import 'package:get/get.dart';
 
 class Profilepage extends StatefulWidget {
@@ -38,114 +39,180 @@ class _ProfilepageState extends State<Profilepage> {
             return const Center(child: Text('Error loading user data'));
           }
           return Scaffold(
-              body: Container(
-            color: Colors.white,
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-            child: Column(
+            body: Column(
               children: [
-                Row(
-                  children: [
-                    const Spacer(),
-                    Center(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 30),
-                        child: Text(
-                          user.name,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            const Spacer(),
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 30),
+                                child: Text(
+                                  user.name,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const Spacer(),
+                            const Icon(Icons.settings),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ClipOval(
+                              child: Image.network(
+                                user.profileImage,
+                                width: 80,
+                                height: 80,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Column(
+                              children: const [
+                                Text(
+                                  "10",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15),
+                                ),
+                                Text(
+                                  "กำลังติดตาม",
+                                  style: TextStyle(color: Colors.black54),
+                                )
+                              ],
+                            ),
+                            const Text("|",
+                                style: TextStyle(
+                                    fontSize: 25, color: Colors.black45)),
+                            Column(
+                              children: const [
+                                Text("20",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15)),
+                                Text(
+                                  "ผู้ติดตาม",
+                                  style: TextStyle(color: Colors.black54),
+                                )
+                              ],
+                            ),
+                            const Text("|",
+                                style: TextStyle(
+                                    fontSize: 25, color: Colors.black45)),
+                            Column(
+                              children: const [
+                                Text("30",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15)),
+                                Text(
+                                  "ถูกใจ",
+                                  style: TextStyle(color: Colors.black54),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Flexible(
+                              child: Text(
+                                user.personalDescription,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.black,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 135, vertical: 10),
+                            textStyle: const TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.bold),
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
                           ),
+                          onPressed: () {
+                            Get.to(() => const EditProfilePage());
+                          },
+                          child: const Text('แก้ไขโปรไฟล์'),
                         ),
-                      ),
+                      ],
                     ),
-                    const Spacer(),
-                    const Icon(Icons.settings),
-                  ],
+                  ),
                 ),
-                const SizedBox(height: 10),
+
+                // แท็บไอคอน
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    ClipOval(
-                      child: Image.network(
-                        user.profileImage,
-                        width: 80,
-                        height: 80,
-                        fit: BoxFit.cover,
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedIndex = 0;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.grid_view,
+                        color: selectedIndex == 0 ? Colors.black : Colors.grey,
                       ),
                     ),
-                    Column(
-                      children: const [
-                        Text(
-                          "10",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        Text(
-                          "กำลังติดตาม",
-                          style: TextStyle(color: Colors.black54),
-                        )
-                      ],
-                    ),
-                    const Text(
-                      "|",
-                      style: TextStyle(fontSize: 25, color: Colors.black45),
-                    ),
-                    Column(
-                      children: const [
-                        Text("20",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 15)),
-                        Text(
-                          "ผู้ติดตาม",
-                          style: TextStyle(color: Colors.black54),
-                        )
-                      ],
-                    ),
-                    const Text(
-                      "|",
-                      style: TextStyle(fontSize: 25, color: Colors.black45),
-                    ),
-                    Column(
-                      children: const [
-                        Text("30",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 15)),
-                        Text(
-                          "ถูกใจ",
-                          style: TextStyle(color: Colors.black54),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(user.personalDescription),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-                      foregroundColor: Colors.black,
-                      padding: const EdgeInsets.only(left: 135, right: 135),
-                      textStyle: const TextStyle(
-                          fontSize: 12, fontWeight: FontWeight.bold),
-                      elevation: 10,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedIndex = 1;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.bookmark_border_outlined,
+                        color: selectedIndex == 1 ? Colors.black : Colors.grey,
                       ),
                     ),
-                    onPressed: () {
-                      Get.to(() => const EditProfilePage());
-                    },
-                    child: const Text('แก้ไขโปรไฟล์')),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedIndex = 2;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.favorite_outline_rounded,
+                        color: selectedIndex == 2 ? Colors.black : Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+
+                Expanded(
+                  child: IndexedStack(
+                    index: selectedIndex,
+                    children: const [
+                      UserMyPostTab(),
+                      UserMyBookmarksTab(),
+                      UserMyLikesTab()
+                    ],
+                  ),
+                )
               ],
             ),
-          ));
+          );
         });
   }
 
@@ -161,6 +228,5 @@ class _ProfilepageState extends State<Profilepage> {
     } else {
       log('Error loading user data: ${response.statusCode}');
     }
-    
   }
 }
---
+++ lib/pages/user_my_likes_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserMyLikesTab extends StatefulWidget {
+  const UserMyLikesTab({super.key});
+
+  @override
+  State<UserMyLikesTab> createState() => _UserMyLikesTabState();
+}
+
+class _UserMyLikesTabState extends State<UserMyLikesTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
